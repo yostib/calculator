c6f5feacd38a8909fb076e4c8931063f8867ad71
YosTgi <yoseph.tibebu@gmail.com>
2021-12-01 21:18:31 +0200

    changes formating

--- calscript.js
+++ calscript.js
@@ -13,7 +13,6 @@
     /*setting up an event listener for all the buttons and finding out which button was
     pressed
     */
-
     buttons.forEach(function (button) {
         button.addEventListener('click', function (e) {
 
@@ -43,6 +42,7 @@
             screen.value = answer;
         }
     })
+    
     /**
      * The event listener works when the clear button is pressed and
      *  it will pass an empty value for the screen
